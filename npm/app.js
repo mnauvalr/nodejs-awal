e30f42a4ee82f41bca4692681fcf3691878e56d7
unknown <mnauvalr27@gmail.com>
2021-06-05 10:14:46 +0700

    penambahan pada file npm

--- npm/app.js
+++ npm/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator.isEmail('mnauvalr27@gmail.com'));
+// console.log(validator.isMobilePhone('081284356219', 'id-ID'));
+// console.log(validator.isNumeric('08128435'));
+
+// console.log(chalk.black.bgBlue('Hello World!'));
+const nama = 'Nauval'
+const pesan = chalk`Lorem, ipsum dolor {bgBlue.black sit amet} adipisicing elit. Nama saya ${nama}.`;
+console.log(pesan);
